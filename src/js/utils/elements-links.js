b7a504dda9a8771718fd7c03551c721a30cb9db0
Nina Samykova <nisik0120@gmail.com>
2025-01-22 14:28:40 +0100

    links to page elements

---
+++ src/js/utils/elements-links.js
@@ -0,0 +1,4 @@
+export const searchGallery = document.getElementById("city-selection");
+export const videoPlayer = document.getElementById("video-player");
+export const locationHeader = document.getElementById("current-location");
+export const searchInput = document.getElementById("search-field");
